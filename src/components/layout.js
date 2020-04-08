255d04fbb89c8cb757de78c928382121aa976eb0
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-04-08 20:11:07 +0200

    feat: Add normalize

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
+import Normalize from "react-normalize";
 
 import { GlobalStyle } from "../global/GlobalStyle";
 
@@ -27,6 +28,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <Normalize />
       <GlobalStyle />
 
       <Header siteTitle={data.site.siteMetadata.title} />
