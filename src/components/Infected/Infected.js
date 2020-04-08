1f1c57f7e5748e72fc794b4c178b8558fa1476f5
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-04-08 20:37:15 +0200

    feat: Add infected component

---
+++ src/components/Infected/Infected.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Styled } from "./Infected.style";
+
+const Infected = () => {
+  return (
+    <Styled.Infected>
+      <h1>1162857</h1>
+    </Styled.Infected>
+  );
+};
+
+export default Infected;
---
+++ src/components/Infected/Infected.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Infected = styled.article`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+`;
+
+export const Styled = {
+  Infected,
+};
