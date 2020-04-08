8e5c7811692357908bf8107745654f832ee20054
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-04-08 20:15:12 +0200

    feat: Add global link style

--- src/global/GlobalStyle.js
+++ src/global/GlobalStyle.js
@@ -1,8 +1,12 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
-import { GlobalBody } from "./styles/body.globalstyle"
+import { GlobalBody } from "./styles/body.globalstyle";
+import { GlobalLink } from "./styles/link.globalstyle";
 
 export const GlobalStyle = createGlobalStyle`
   /** Body */
   ${GlobalBody}
-`
+
+  /** Link */
+  ${GlobalLink}
+`;
---
+++ src/global/styles/link.globalstyle.js
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+// Tokens
+import { token } from "../../utils/tokens/index";
+
+export const GlobalLink = css`
+  a {
+    color: ${token.color.neutral.TINT_90};
+  }
+`;
