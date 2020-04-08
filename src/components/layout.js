626c92406223b831711a26b0e4501b1384746cc8
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-04-08 20:01:19 +0200

    feat: Add global style to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,14 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from "react";
+import PropTypes from "prop-types";
+import { useStaticQuery, graphql } from "gatsby";
 
-import Header from "./header"
-import "./layout.css"
+import { GlobalStyle } from "../global/GlobalStyle";
+
+import Header from "./header";
+// import "./layout.css";
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,18 +23,19 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
+      <GlobalStyle />
+
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+        }}>
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
@@ -41,11 +44,11 @@ const Layout = ({ children }) => {
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
