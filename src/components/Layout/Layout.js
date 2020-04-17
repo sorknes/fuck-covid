ae53528716a897fd7c03df04ce045c8839c72a9c
Knut Sorknes <knut.sorknes@makingwaves.com>
2020-04-17 19:28:49 +0200

    feat: Start with a clean canvas

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,40 +7,40 @@
 
 import React from "react";
 import PropTypes from "prop-types";
-import { useStaticQuery, graphql } from "gatsby";
+// import { useStaticQuery, graphql } from "gatsby";
 
 // Global style
 import "../../styles/scss/fc-main.scss";
 
-import Header from "../header";
+// import Header from "../header";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `);
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      {/* <div
         style={{
           margin: `0 auto`,
           maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
-        }}>
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        }}> */}
+      <main>{children}</main>
+      {/* <footer>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.org">Gatsby</a>
+      </footer> */}
+      {/* </div> */}
     </>
   );
 };
